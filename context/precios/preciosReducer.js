600a1ea01709cf4f4cff677c012241f31a29113a
Francisco Micucci <fmicucci@piconsulting.com.ar>
2023-11-29 13:31:59 -0300

    Define variables on the top

--- context/precios/preciosReducer.js
+++ context/precios/preciosReducer.js
@@ -1,43 +1,45 @@
 import {
-	TRAER_PRECIOS,
-	LISTA_PRECIOS,
-	ERROR_PRECIOS,
-	FILAS_PRECIOS,
-} from '../../types';
-import { filtro } from '../../functions/filtros.js';
+  TRAER_PRECIOS,
+  LISTA_PRECIOS,
+  ERROR_PRECIOS,
+  FILAS_PRECIOS,
+} from "../../types";
+import { filtro } from "../../functions/filtros.js";
 
 const PreciosReducer = (state, action) => {
-	switch (action.type) {
-		case TRAER_PRECIOS:
-			let vars = { lisPre: state.lista, bus: action.payload.bus };
-			let r = filtro(action.payload.arrayProd, vars);
-			return {
-				...state,
-				precios: action.payload.arrayProd,
-				filas: r,
-				cargando: false,
-			};
-		case LISTA_PRECIOS:
-			return {
-				...state,
-				lista: action.payload,
-			};
-		case FILAS_PRECIOS:
-			vars = { lisPre: state.lista, bus: action.payload };
-			r = filtro(state.precios, vars);
-			return {
-				...state,
-				filas: r,
-			};
-		case ERROR_PRECIOS:
-			return {
-				...state,
-				mensaje: action.payload,
-			};
+  let vars = {};
+  let r = [];
+  switch (action.type) {
+    case TRAER_PRECIOS:
+      vars = { lisPre: state.lista, bus: action.payload.bus };
+      r = filtro(action.payload.arrayProd, vars);
+      return {
+        ...state,
+        precios: action.payload.arrayProd,
+        filas: r,
+        cargando: false,
+      };
+    case LISTA_PRECIOS:
+      return {
+        ...state,
+        lista: action.payload,
+      };
+    case FILAS_PRECIOS:
+      vars = { lisPre: state.lista, bus: action.payload };
+      r = filtro(state.precios, vars);
+      return {
+        ...state,
+        filas: r,
+      };
+    case ERROR_PRECIOS:
+      return {
+        ...state,
+        mensaje: action.payload,
+      };
 
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
 };
 
 export default PreciosReducer;
